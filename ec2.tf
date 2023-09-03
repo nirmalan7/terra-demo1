3f97bea896ed0fba06262775e5814df8bbe117f2
nirmalan7 <132876983+nirmalan7@users.noreply.github.com>
2023-09-03 18:40:29 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIAVQPBNTQ6NNUGZZ7S"
-  secret_key = "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"
+  access_key = "none"
+  secret_key = "none
 }
 resource "aws_instance" "Myfirst-ec2-instance-terra" {
     ami           = "ami-024e6efaf93d85776"
